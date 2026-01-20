0d668e7f97ba825a7a7172d9b864efe4cb57e68e
mateus <mateus.borja@ufpi.edu.br>
2026-01-20 11:19:13 -0300

    add vercel config

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 
 app.include_router(entry_root)
 app.include_router(publicacoes_root)
+
+# This is important for Vercel
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
